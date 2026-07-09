34459f00cc753efc7a6619ce673fbf71adc32991
vansh1567 <mr.shanu2002@gmail.com>
2026-07-09 05:42:28 +0530

    forth

---
+++ talwindprops/src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import Card from './components/Card'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='mb-4 bg-green-400 text-black p-4 rounded-xl'>tailwind test</h1>
+      <Card/>
+      <Card/>
+      
+    
+    </>
+  )
+}
+
+export default App
